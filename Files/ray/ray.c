a5350ee985ae4975109c3800bc69b3326442c093
Frankiwi20 <115595456+Frankiwi20@users.noreply.github.com>
2024-05-27 19:58:11 -0700

    Add files via upload

--- Files/ray/ray.c
+++ Files/ray/ray.c
@@ -1,1 +1,22 @@
+#include "ray.h"
 
+// Function to create a Ray given an origin and direction.
+Ray
+ray (Tuple origin, Tuple direction)
+{
+  Ray r;
+  r.origin = origin;
+  r.direction = direction;
+  return r;
+}
+
+
+
+// Function to compute the position along the ray at distance t
+Tuple
+position (Ray r, double t)
+{
+  // The point on the ray is computed as: origin + t * direction
+  Tuple scaled_direction = scalarMultTuple (t, r.direction);
+  return addTuples (r.origin, scaled_direction);
+}
